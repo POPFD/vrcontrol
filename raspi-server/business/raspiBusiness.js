14b0de74ef60180259d4ae894f8742c2cf26fba0
Damon Jones <damon.mark.jones@outlook.com>
2017-03-13 16:18:11 +0000

    Add logic to check for direction command

--- raspi-server/business/raspiBusiness.js
+++ raspi-server/business/raspiBusiness.js
@@ -1,16 +1,54 @@
 (function (raspi) {
 
+    const forward = "forward";
+    const backward = "backward";
+    const left = "left";
+    const right = "right";
+    const stop = "stop";
+    const invalidCommand = "Command not recognised available commands are: 'forward', 'backward', 'left', 'right', 'stop'."
+
     raspi.moveZumo = function (direction, callback) {
+        // Get current time 
+        let datetime = new Date(new Date().getTime()).toLocaleTimeString();
+
         if (direction) {
-            // Use cylon.js library here
-            var result = "Sending direction command to zumo arduino";
+            var result = invalidCommand;
+
+            // Check direction
+            if (direction === forward) {
+                // Call zumo motors library 
+
+                result = "Moving zumo forwards";
+            } else if (direction === backward) {
+                // Call zumo motors library 
+
+                result = "Moving zumo backwards";
+            } else if (direction === left) {
+                // Call zumo motors library 
+
+                result = "Turning zumo to the left";
+            } else if (direction === right) {
+                // Call zumo motors library 
+
+                result = "Turning zumo to the right";
+            } else if (direction === stop) {
+                // Call zumo motors library 
+
+                result = "Stopping zumo";
+            }
 
             callback(null, result);
+            
+            var logMessage = `${datetime} - ${result}`;
+            console.log(logMessage);
         } else {
             // Error occurred no direction was present
-            var err = "Could not talk to zumo arduino";
+            var err = "Direction parameter was not supplied";
             
             callback(err, null);
+
+            var logMessage = `${datetime} - ${result}`;
+            console.log(logMessage);
         }
     };
 
@@ -18,4 +56,4 @@
         
     };
 
-})(module.exports);
+})(module.exports); 
