b49cc1b9589950d63234f39104f09fb7d8269cf1
Jack Allister <b3042098@my.shu.ac.uk>
2017-03-13 14:29:23 +0000

    May have fixed exports for commands of robot

--- raspi-server/business/zumoMotors.js
+++ raspi-server/business/zumoMotors.js
@@ -12,51 +12,51 @@ Cylon.robot({
     dirR: { driver: 'direct-pin', pin: 7 }
   },
 
-  setLeftSpeed: function(speed) {
-    var reverse = 0;
+  commands: {
+    setLeftSpeed: function(speed) {
+      var reverse = 0;
 
-    /* Set reverse variable if speed is a negative */
-    if (speed < 0) {
-      speed = -speed;
-      reverse = 1;
+      /* Set reverse variable if speed is a negative */
+      if (speed < 0) {
+        speed = -speed;
+        reverse = 1;
+      }
+
+      /* Stop from exceeding max speed */
+      if (speed > 400)
+        speed = 400;
+
+      /* Set the speed/PWM for right motor */
+      this.pwmL.analogWrite(speed * 51 / 80);
+
+      /* Sets the direction pin for forward/backward */
+      this.dirL.digitalWrite(reverse);
+    },
+
+    setRightSpeed: function(speed) {
+      var reverse = 0;
+
+      /* Set reverse variable if speed is a negative */
+      if (speed < 0) {
+        speed = -speed;
+        reverse = 1;
+      }
+
+      /* Stop from exceeding max speed */
+      if (speed > 400)
+        speed = 400;
+
+      /* Set the speed/PWM for right motor */
+      this.pwmR.analogWrite(speed * 51 / 80);
+
+      /* Sets the direction pin for forward/backward */
+      this.dirR.digitalWrite(reverse);
     }
-
-    /* Stop from exceeding max speed */
-    if (speed > 400)
-      speed = 400;
-
-    /* Set the speed/PWM for right motor */
-    this.pwmL.analogWrite(speed * 51 / 80);
-
-    /* Sets the direction pin for forward/backward */
-    this.dirL.digitalWrite(reverse);
-  },
-
-  setRightSpeed: function(speed) {
-
-    var reverse = 0;
-
-    /* Set reverse variable if speed is a negative */
-    if (speed < 0) {
-      speed = -speed;
-      reverse = 1;
-    }
-
-    /* Stop from exceeding max speed */
-    if (speed > 400)
-      speed = 400;
-
-    /* Set the speed/PWM for right motor */
-    this.pwmR.analogWrite(speed * 51 / 80);
-
-    /* Sets the direction pin for forward/backward */
-    this.dirR.digitalWrite(reverse);
   },
 
   work: function(my) {
     /* Do nothing as of yet */
-    console.log(this);
   }
 }).start();
 
-module.exports = Cylon.robot;
+module.exports = Cylon.robot.commands;
