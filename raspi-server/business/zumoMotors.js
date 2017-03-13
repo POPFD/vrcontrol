e5392fff8baf9a60862685792b6213d00d2e8a10
Jack Allister <b3042098@my.shu.ac.uk>
2017-03-13 15:27:50 +0000

    Refactored commands again

--- raspi-server/business/zumoMotors.js
+++ raspi-server/business/zumoMotors.js
@@ -14,46 +14,44 @@ Cylon.robot({
     dirR: { driver: 'direct-pin', pin: 7 }
   },
 
-  commands: {
-    setLeftSpeed: function(speed) {
-      var reverse = 0;
+  setLeftSpeed: function(speed) {
+    var reverse = 0;
 
-      /* Set reverse variable if speed is a negative */
-      if (speed < 0) {
-        speed = -speed;
-        reverse = 1;
-      }
-
-      /* Stop from exceeding max speed */
-      if (speed > 400)
-        speed = 400;
-
-      /* Set the speed/PWM for right motor */
-      this.pwmL.analogWrite(speed * 51 / 80);
-
-      /* Sets the direction pin for forward/backward */
-      this.dirL.digitalWrite(reverse);
-    },
-
-    setRightSpeed: function(speed) {
-      var reverse = 0;
-
-      /* Set reverse variable if speed is a negative */
-      if (speed < 0) {
-        speed = -speed;
-        reverse = 1;
-      }
-
-      /* Stop from exceeding max speed */
-      if (speed > 400)
-        speed = 400;
-
-      /* Set the speed/PWM for right motor */
-      this.pwmR.analogWrite(speed * 51 / 80);
-
-      /* Sets the direction pin for forward/backward */
-      this.dirR.digitalWrite(reverse);
+    /* Set reverse variable if speed is a negative */
+    if (speed < 0) {
+      speed = -speed;
+      reverse = 1;
     }
+
+    /* Stop from exceeding max speed */
+    if (speed > 400)
+      speed = 400;
+
+    /* Set the speed/PWM for right motor */
+    this.pwmL.analogWrite(speed * 51 / 80);
+
+    /* Sets the direction pin for forward/backward */
+    this.dirL.digitalWrite(reverse);
+  },
+
+  setRightSpeed: function(speed) {
+    var reverse = 0;
+
+    /* Set reverse variable if speed is a negative */
+    if (speed < 0) {
+      speed = -speed;
+      reverse = 1;
+    }
+
+    /* Stop from exceeding max speed */
+    if (speed > 400)
+      speed = 400;
+
+    /* Set the speed/PWM for right motor */
+    this.pwmR.analogWrite(speed * 51 / 80);
+
+    /* Sets the direction pin for forward/backward */
+    this.dirR.digitalWrite(reverse);
   },
 
   work: function(my) {
