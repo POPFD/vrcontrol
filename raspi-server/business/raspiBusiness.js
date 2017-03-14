9cddfd491239f9ff7552501c258e0771e29ebf40
Damon Jones <damon.mark.jones@outlook.com>
2017-03-14 21:42:58 +0000

    Refactor if else to a switch statement and rename constant variables in raspiBusiness

--- raspi-server/business/raspiBusiness.js
+++ raspi-server/business/raspiBusiness.js
@@ -1,92 +1,95 @@
 (function (raspi) {
     var zumoInterface = require('../business/zumoInterface');
-    const forward = "forward";
-    const backward = "backward";
-    const left = "left";
-    const right = "right";
-    const stop = "stop";
-    const invalidCommand = "Command not recognised available commands are: 'forward', 'backward', 'left', 'right', 'stop'."
+    const FORWARD = "forward";
+    const BACKWARD = "backward";
+    const LEFT = "left";
+    const RIGHT = "right";
+    const STOP = "stop";
+    const INVALID_COMMAND = "Command not recognised available commands are: 'forward', 'backward', 'left', 'right', 'stop'."
+    const NO_PARAMETER = "Direction parameter was not supplied";
 
     raspi.moveZumo = function (direction, callback) {
         // Get current time 
         let datetime = new Date(new Date().getTime()).toLocaleTimeString();
+        let result;
 
         if (direction) {
-            let result = invalidCommand;
+            switch (direction) {
+                case FORWARD:
+                    // Call zumo interface to send command
+                    zumoInterface.moveForward(function (err) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            result = "Moving zumo forwards";
+                            console.log(`${datetime} - ${result}`);
 
-            // Check direction
-            if (direction === forward) {
-                // Call zumo interface to send command
-                zumoInterface.moveForward(function (err) {
-                    if (err) {                        
-                        callback(err, null);
-                    } else {
-                        result = "Moving zumo forwards";
-                        console.log(`${datetime} - ${result}`);
+                            callback(null, result);
+                        }
+                    });
+                    break;
+                case BACKWARD:
+                    // Call zumo interface to send command
+                    zumoInterface.moveBackward(function (err) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            result = "Moving zumo backwards";
+                            console.log(`${datetime} - ${result}`);
 
-                        callback(null, result);
-                    }
-                });                
-            } else if (direction === backward) {
-                // Call zumo interface to send command
-                zumoInterface.moveBackward(function (err) {
-                    if (err) {
-                        callback(err, null);
-                    } else {
-                        result = "Moving zumo backwards";
-                        console.log(`${datetime} - ${result}`);
+                            callback(null, result);
+                        }
+                    });   
+                    break;
+                case LEFT:
+                    // Call zumo interface to send command
+                    zumoInterface.moveLeft(function (err) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            result = "Turning zumo to the left";
+                            console.log(`${datetime} - ${result}`);
 
-                        callback(null, result);
-                    }
-                });                     
-            } else if (direction === left) {
-                // Call zumo interface to send command
-                zumoInterface.moveLeft(function (err) {
-                    if (err) {
-                        callback(err, null);
-                    } else {
-                        result = "Turning zumo to the left";
-                        console.log(`${datetime} - ${result}`);
+                            callback(null, result);
+                        }
+                    });
+                    break;
+                case RIGHT:
+                    // Call zumo interface to send command
+                    zumoInterface.moveRight(function (err) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            result = "Turning zumo to the right";
+                            console.log(`${datetime} - ${result}`);
 
-                        callback(null, result);
-                    }
-                });                      
-            } else if (direction === right) {
-                // Call zumo interface to send command
-                zumoInterface.moveRight(function (err) {
-                    if (err) {
-                        callback(err, null);
-                    } else {
-                        result = "Turning zumo to the right";
-                        console.log(`${datetime} - ${result}`);
+                            callback(null, result);
+                        }
+                    });
+                    break;
+                case STOP:
+                    // Call zumo interface to send command
+                    zumoInterface.stopMovement(function (err) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            result = "Stopping zumo";
+                            console.log(`${datetime} - ${result}`);
 
-                        callback(null, result);
-                    }
-                });                         
-            } else if (direction === stop) {
-                // Call zumo interface to send command
-                zumoInterface.stopMovement(function (err) {
-                    if (err) {
-                        callback(err, null);
-                    } else {
-                        result = "Stopping zumo";
-                        console.log(`${datetime} - ${result}`);
+                            callback(null, result);
+                        }
+                    });
+                    break;
+                default:
+                    console.log(`${datetime} - ${INVALID_COMMAND}`);
 
-                        callback(null, result);
-                    }
-                });             
-            } else {
-                var err = invalidCommand;
-                console.log(`${datetime} - ${err}`);
-
-                callback(err, null);
-            }            
+                    callback(INVALID_COMMAND, null);
+            }           
         } else {
             // Error occurred no direction was present
-            var err = "Direction parameter was not supplied";
-            console.log(`${datetime} - ${err}`);
+            console.log(`${datetime} - ${NO_PARAMETER}`);
 
-            callback(err, null);            
+            callback(NO_PARAMETER, null);            
         }
     };
 })(module.exports); 
